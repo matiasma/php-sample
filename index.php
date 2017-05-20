674665ce7792cfe5f11453457c0c83eb8a1360a6
matiasma <matias_marcelo@live.com>
2017-05-20 19:58:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>VM Scale Set</title>
+<style>
+table {
+  font-family: arial;
+  font-size: 20px;
+}
+td {
+  text-align: left;
+  vertical-align: middle;
+  width: 100px;
+}
+th {
+  text-align: left;
+  vertical-align: middle;
+}
+</style>
+</head>
+<body>
+
+<?php
+echo '<table><tr><td>PHP: </td><th>' . phpversion() . '</th></tr>';
+echo '<tr><td>SERVER: </td><th>' . gethostname() . '</th></tr></table>';
+?>
+</body>
+</html>
